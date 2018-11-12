ab5ac497afdec56c178735491aee2d0a0be10f16
shmuelS <shmuelshmila0@gmail.com>
2018-11-12 17:29:10 +0200

    build recipe-detail component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,6 +9,7 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
+    new Recipe('A Test Recipe' , 'This is simply a test', 'https://get.pxhere.com/photo/table-wood-rustic-dish-meal-food-green-mediterranean-red-cooking-culinary-garlic-produce-vegetable-natural-fresh-brown-italy-baking-gourmet-healthy-dessert-lunch-cuisine-sauce-delicious-homemade-onion-preparation-dough-basil-pizza-cheese-pesto-cook-tomato-cutting-baked-dinner-wooden-tasty-tomatoes-vegetarian-cutting-board-quiche-fragrant-diet-herbs-italian-traditional-flour-mozzarella-parchment-paper-salt-shaker-parmesan-cheese-arugula-pizza-cutter-salt-grinder-sundried-tomatoes-608902.jpg'),
     new Recipe('A Test Recipe' , 'This is simply a test', 'https://get.pxhere.com/photo/table-wood-rustic-dish-meal-food-green-mediterranean-red-cooking-culinary-garlic-produce-vegetable-natural-fresh-brown-italy-baking-gourmet-healthy-dessert-lunch-cuisine-sauce-delicious-homemade-onion-preparation-dough-basil-pizza-cheese-pesto-cook-tomato-cutting-baked-dinner-wooden-tasty-tomatoes-vegetarian-cutting-board-quiche-fragrant-diet-herbs-italian-traditional-flour-mozzarella-parchment-paper-salt-shaker-parmesan-cheese-arugula-pizza-cutter-salt-grinder-sundried-tomatoes-608902.jpg')
   ];
 
