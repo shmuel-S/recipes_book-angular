bb916b60ca045a63bb46e562f61fcf9e78b2c65c
shmuelS <shmuelshmila0@gmail.com>
2018-11-14 16:14:11 +0200

    build recipe-detail component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 
@@ -8,14 +8,19 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter <Recipe>();
   recipes: Recipe[] = [
     new Recipe('A Test Recipe' , 'This is simply a test', 'https://get.pxhere.com/photo/table-wood-rustic-dish-meal-food-green-mediterranean-red-cooking-culinary-garlic-produce-vegetable-natural-fresh-brown-italy-baking-gourmet-healthy-dessert-lunch-cuisine-sauce-delicious-homemade-onion-preparation-dough-basil-pizza-cheese-pesto-cook-tomato-cutting-baked-dinner-wooden-tasty-tomatoes-vegetarian-cutting-board-quiche-fragrant-diet-herbs-italian-traditional-flour-mozzarella-parchment-paper-salt-shaker-parmesan-cheese-arugula-pizza-cutter-salt-grinder-sundried-tomatoes-608902.jpg'),
-    new Recipe('A Test Recipe' , 'This is simply a test', 'https://get.pxhere.com/photo/table-wood-rustic-dish-meal-food-green-mediterranean-red-cooking-culinary-garlic-produce-vegetable-natural-fresh-brown-italy-baking-gourmet-healthy-dessert-lunch-cuisine-sauce-delicious-homemade-onion-preparation-dough-basil-pizza-cheese-pesto-cook-tomato-cutting-baked-dinner-wooden-tasty-tomatoes-vegetarian-cutting-board-quiche-fragrant-diet-herbs-italian-traditional-flour-mozzarella-parchment-paper-salt-shaker-parmesan-cheese-arugula-pizza-cutter-salt-grinder-sundried-tomatoes-608902.jpg')
+    new Recipe('Another Test Recipe' , 'This is simply a test', 'https://get.pxhere.com/photo/table-wood-rustic-dish-meal-food-green-mediterranean-red-cooking-culinary-garlic-produce-vegetable-natural-fresh-brown-italy-baking-gourmet-healthy-dessert-lunch-cuisine-sauce-delicious-homemade-onion-preparation-dough-basil-pizza-cheese-pesto-cook-tomato-cutting-baked-dinner-wooden-tasty-tomatoes-vegetarian-cutting-board-quiche-fragrant-diet-herbs-italian-traditional-flour-mozzarella-parchment-paper-salt-shaker-parmesan-cheese-arugula-pizza-cutter-salt-grinder-sundried-tomatoes-608902.jpg')
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+
   }
 
 }
