f929327c99689a67a67494529c498d17dde12352
shmuelS <shmuelshmila0@gmail.com>
2018-12-27 16:10:41 +0200

    add signup route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   loadedFeature = 'recipe';
-  
+
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyDtSW26c51uiGlbJodeVjyd-cSidCkfxYM',
+      authDomain: 'recipes-book-angular-1de01.firebaseapp.com'
+    });
+  }
+
   onNavigate(feature: string) {
     this.loadedFeature = feature;
   }
