3e0d189fe339c140066a69ddd34b9270419878b2
shmuelS <shmuelshmila0@gmail.com>
2018-12-29 16:27:34 +0200

    update data-storage-service

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import * as firebase from 'firebase';
 
 export class AuthService {
+
   signupUser(email: string, password: string) {
     firebase.auth().createUserWithEmailAndPassword(email, password)
       .catch(
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,23 +1,45 @@
 import {Injectable} from '@angular/core';
 import {Http , Response} from '@angular/http';
 
+import { map } from 'rxjs/operators';
 import {RecipeService} from '../recipes/recipe.service';
 import {Recipe} from '../recipes/recipe.model';
 
 @Injectable()
 export class DataStorageService {
-  constructor(private http: Http, private recipeService: RecipeService) {}
+  constructor(private http: Http,
+              private recipeService: RecipeService) {}
 
   storeRecipes() {
     return this.http.put('https://recipes-book-angular-1de01.firebaseio.com/recipes.json',
-                      this.recipeService.getRecipes());
+           this.recipeService.getRecipes());
   }
+
+  // getRecipes() {
+  //   this.http.get('https://recipes-book-angular-1de01.firebaseio.com/recipes.json')
+  //     .subscribe(
+  //       (response: Response) => {
+  //         const recipes: Recipe[] = response.json();
+  //         this.recipeService.setRecipes(recipes);
+  //       }
+  //     );
+  // }
 
   getRecipes() {
     this.http.get('https://recipes-book-angular-1de01.firebaseio.com/recipes.json')
-      .subscribe(
+      .pipe(map(
         (response: Response) => {
           const recipes: Recipe[] = response.json();
+          for (const recipe of recipes) {
+            if (!recipe['ingredients']) {
+              recipe['ingredients'] = [];
+            }
+          }
+          return recipes;
+        }
+      ))
+      .subscribe(
+        (recipes: Recipe[]) => {
           this.recipeService.setRecipes(recipes);
         }
       );
