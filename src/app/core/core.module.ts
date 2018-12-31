f85f0118b7fc14cc5a989922319b37003e5fcc67
shmuelS <shmuelshmila0@gmail.com>
2018-12-31 14:18:31 +0200

    add logging interceptor

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -10,7 +10,9 @@ import {ShoppingListService} from '../shopping-list/shopping-list.service';
 import {RecipeService} from '../recipes/recipe.service';
 import {DataStorageService} from '../shared/data-storage.service';
 import {AuthService} from '../auth/auth.service';
+
 import {AuthInterceptor} from '../shared/auth.interceptor';
+import {LoggingInterceptor} from '../shared/logging.interceptor';
 
 
 @NgModule({
@@ -31,7 +33,8 @@ import {AuthInterceptor} from '../shared/auth.interceptor';
     RecipeService,
     DataStorageService,
     AuthService,
-    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}
+    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: LoggingInterceptor, multi: true}
   ]
 })
 export class CoreModule {
